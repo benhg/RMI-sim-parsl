e3ae9f77bdb4857bae50b7e7c83a48408bea0611
Ben Glick <glick@lclark.edu>
2019-04-05 12:22:19 -0700

    starting to split out config from code, also starting to run simulations as parsl bash apps

---
+++ generate_config.py
@@ -0,0 +1,68 @@
+def ask_user_for_info():
+    # Begin parameter collection
+
+    print("Enter the following parameters:")
+
+    # Determine desired model
+
+    models = {'Ising': 'Ising_model', 'XY': 'XY_model', 'QCD': 'QCD_model', 'beta':'beta_model'}
+    model = input("Enter model to simulate: ")
+    while model not in models:
+        print("Valid models are:", list(models.keys()))
+        model = input("Model entered was invalid. Retry: ")
+    print(models[model])
+    # Based on model, determine y_tilde and theta coefficient
+
+    if model == 'Ising' or 'XY' or 'beta':
+        y_tilde = 0
+        theta_coefficient = 0
+    if model == 'QCD':
+        y_tilde = int(input("Enter y_tilde: "))
+        theta_coefficient = int(input("Enter the theta coefficient: "))
+
+
+    n_valid = [2]
+    n = int(input("Enter replica number: "))
+    while n not in n_valid:
+        n = int(input("Invalid replica number. Retry: "))
+
+    # Get the lattice size
+
+    size = int(input("Enter linear lattice size: "))
+    while size % 8 != 0:
+        size = int(input("Please enter a multiple of 8: "))
+
+    # Get delta_T
+
+    delta_T = float(input("Enter temperature step size: "))
+
+    # Get number of independent measurements
+
+    measurements = int(input("Enter number of independent measurements: "))
+
+    calc_file = "N={0}x{0},{1}M,{2}dT,n={3}".format(size, measurements, delta_T, n)
+    path = os.path.dirname(os.path.realpath(__file__))
+    print("Ready to populate calculation file: ", calc_file)
+
+    if model == 'beta':
+        T_start = float(input("Enter starting beta: "))
+        T_end = float(input("Enter final beta: "))
+        T_batch = float(input("Enter batch beta step size: "))
+    else:
+        T_start = float(input("Enter starting T: "))
+        T_end = float(input("Enter final T: "))
+        T_batch = float(input("Enter batch T step size: "))
+
+    model_folder = path
+
+
+    # Script name format
+
+    scripts = {'XY':'RMI_{0}.py'.format(model), 'QCD':'RMI_{0}.py'.format(model), 'beta':'RMI_{0}.py'.format(model)}
+
+    # Folder paths
+
+    packed_folder = 'N={0}x{0},{1}M,{2}dT,n={3}'.format(size, measurements,
+                                                                   delta_T, n)
+    whole_path = '{0}/{1}/{2}dT/{3}'.format(path, model_folder, delta_T, packed_folder)
+    folder_path = '{0}/{1}/{2}dT/{3}'.format(path, model_folder, delta_T, packed_folder)
---
+++ orchestrator.py
@@ -0,0 +1,58 @@
+# Master program to make everything run
+import json 
+import sys
+
+import parsl
+from numpy import *
+from parsl.configs.local_threads import config
+
+import generate_config
+
+
+# basic config
+parsl.load(config)
+
+# Make sure environment is set up
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+else:
+    shutil.rmtree(folder_path)
+    os.makedirs(folder_path)
+
+
+def temp_spread(t_min, t_max, T_ba, dT):
+    # Stores each temp pair as a tuple
+    t_s = arange(t_min, t_max, T_ba)
+    count = len(t_s)
+    range_list = []
+    for i in range(count):
+        range_list.append((t_s[i] + dT, t_s[i] + T_ba))
+    return range_list
+
+
+@bash_app
+def run_single_temp_range(temp_pair):
+    write_T_min = round(temp_pair[0], 3)
+    write_T_max = round(temp_pair[1], 3)
+    # Now generate:
+    return "python3 {11}/{5}/Master_Codes/{12} {0} {6} {7} {2} {3} {8} {10} {11}".format(size, n, write_T_min, write_T_max, model, model_folder,
+            delta_T, measurements, y_tilde, whole_path, theta_coefficient, path, scripts[model])
+
+
+def submit_all_jobs(temp_list):
+    script_num = 1
+    for temp_pair in temp_list:
+        run_single_temp_range(temp_pair)
+
+
+if __name__ == '__main__':
+    conf_file = sys.argv[1]
+    run_config = {}
+    try:
+        with open(conf_file) as fh:
+            run_config = json.load(fh)
+    except Exception as e:
+        run_config = generate_config.ask_user_for_info()
+
+    temp_list = array(temp_spread(T_start, T_end, T_batch, delta_T))
+    submit_all_jobs(temp_list)
